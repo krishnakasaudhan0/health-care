2a760c4f085296612e08e532a7169d5cc6c4d319
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-04-15 14:22:05 +0000

    feat: add patient vitals entry forms

--- src/forms.js
+++ src/forms.js
@@ -4,3 +4,5 @@
 // Git contribution logic update at: 2026-04-11T05:05:17.000Z [hash: qhopc]
 
 // Git contribution logic update at: 2026-04-14T12:16:31.000Z [hash: 4btgb6]
+
+// Git contribution logic update at: 2026-04-15T14:22:05.000Z [hash: juwux]
